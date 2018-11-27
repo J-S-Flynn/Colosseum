01496f71d76c6bd41bc6f1dedcbde71afbe9e629
Jason Sebastian Flynn <30478271+TheoXIII@users.noreply.github.com>
2018-11-27 08:45:22 +0000

    Add files via upload

---
+++ colosseum/src/main/java/com/jsf/Main.java
@@ -0,0 +1,11 @@
+package com.jsf;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SpringBootApplication
+public class Main {
+
+
+}
